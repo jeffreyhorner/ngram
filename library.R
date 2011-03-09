84b072a1b422ca28f9432e31e2c2e41bf4fc981a
hornerj <hornerj@ip-10-251-43-212.ec2.internal>
2011-03-09 11:56:10 +0000

    Created rApache handler to serve ngram queries

--- library.R
+++ library.R
@@ -1,11 +1,21 @@
 library(rmemcache)
-# Create a list of connections to all memcache searvers
-m <- lapply(readLines('memcache_ip.txt'),function(x)  mcConnect(paste(x,':11211',sep='')))
+
+# Change these to files that contain IP addresses, one per line
+MEMCACHE_IPS<-'/home/hornerj/memcache_ip.txt'
+MEMCACHE_IPS_1GRAM<-'/home/hornerj/memcache_ip_1gram.txt'
+ips <- readLines(MEMCACHE_IPS)
+ips <- c(ips,readLines(MEMCACHE_IPS_1GRAM))
+
+# Create a list of connections to all memcache servers
+m <- lapply(ips,function(x)  mcConnect(paste(x,':11211',sep='')))
 eskape <- function(key) gsub(' ','|',key)
 
-query_bigram <- function(key,counts=c('match','page','volume'),years=c(1890,2008)) {
+query_ngram <- function(key,counts=c('match','page','volume'),years=c(1890,2008),na.rm=FALSE) {
 	x <- unlist(lapply(m,function(x) mcGet(x,eskape(key))))
-	if (is.null(x)) return(NULL)
+	if (is.null(x)){
+		if (na.rm) return(NULL)
+		else return(rep(NA,years[2]-years[1]+1))
+	}
 	as.integer(strsplit(x,' ')[[1]])[seq(years[1]-1889,years[2]-1889)]
 }
 
@@ -13,21 +23,57 @@ parse_to_ngram <- function(x,ngrams=c(1,2,3,4,5)){
 	x <- strsplit(paste(x,collapse=' '),'[\n\t ]+',perl=TRUE)[[1]]
 	x <- x[x!='']
 
-	if (ngrams[1] == 1)
-		x
-	else if (ngrams[1] == 2){
+	if (ngrams[1] == 2){
 		len <- length(x)
 		lm1 <- len - 1
-		paste(x[1:lm1],x[2:len],sep=' ')
+		return(paste(x[1:lm1],x[2:len],sep=' '))
 	}
+
+	# Default case ngrams[1]==1
+	x
 }
 
-bigram_matches <- function(Text){
-	x <- unlist(lapply(parse_to_ngram(Text,ngrams=2),query_bigram))
+ngram_matches <- function(Text,na.rm=FALSE){
+	x <- unlist(lapply(Text,query_ngram,na.rm=na.rm))
 	dim(x) <- c(119,length(x)/119)
+
+	if (!na.rm) attr(x,'ngrams') <- Text
+	x
+}
+
+bigram_matches <- function(Text,na.rm=FALSE){
+	ngrams <- parse_to_ngram(Text,ngrams=2)
+	x <- unlist(lapply(ngrams,query_ngram,na.rm=na.rm))
+	dim(x) <- c(119,length(x)/119)
+
+	if (!na.rm) attr(x,'ngrams') <- ngrams
 	x
 }
 
 # To sum over all matches use
 # rowSums(bigram_matches())
 
+rApache_handler <- function(){
+	ngram <- 0
+	if(!is.null(GET$ngram))
+		ngram <- as.integer(GET$ngram)
+
+	if (ngram < 0 || ngram > 2)
+		ngram <- 0
+
+	text <- GET$text
+	if (is.null(text))
+		text <- POST$text
+	if (is.null(text)) return(OK)
+
+	if (ngram==0)
+		text <- strsplit(text,',')[[1]]
+	else
+		text <- parse_to_ngram(text,ngrams=ngram)
+
+	x <- ngram_matches(text)
+	y <- data.frame(x)
+	names(y) <- attr(x,'ngrams')
+	write.csv(y,row.names=FALSE)
+	OK
+}
